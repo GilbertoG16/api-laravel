66d978fdec0f96c28ce76009e0147bc7d0a29732
GilbertoG16 <112657690+GilbertoG16@users.noreply.github.com>
2023-10-17 00:11:09 -0500

    Añadiendo la relación por qr
    Añadí la relación por qr en base a que pueda verse el showOne de dos formas, por la id del LearningInformation o por la uniqueId del qr, estas dos son bien diferentes debido a que la del Learning es un simple id autoincremental y la del qr es una uniqueId que creo en base al nombre de el learning su ubicación y el número de qr que viene siendo, si se repite el nombre en la base de datos este le añade un unique example: Sopa_2_2_652e163894903, la original: Sopa_1_2.

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable implements MustVerifyEmail
     public function profile(){
         return $this->hasOne(Profile::class);
     }
+
+    public function userQrHistories()
+    {
+        return $this->hasMany(UserQrHistory::class);
+    }
 }
