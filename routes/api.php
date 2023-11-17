94dc0ffb221f86ebb42b3967b8afc90d2e8d9caf
GilbertoG16 <112657690+GilbertoG16@users.noreply.github.com>
2023-11-16 21:02:13 -0500

    son varia cosa junta
    offi broki

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,8 @@ Route::middleware('auth:sanctum', 'verified', 'status')->group(function () {
     Route::post('/store-fcm-token', [FcmTokenController::class, 'storeFcmToken']);
     Route::get('/profile', [UserController::class, 'userProfile']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    Route::get('/user/appointments', [AppointmentController::class, 'getUserAppointments']);
+    Route::delete('/user/appointments/{id}', [AppointmentController::class, 'cancelUserAppointment']);
     Route::get('/ranking', [TriviaController::class, 'indexScore']);
 
     Route::get('/user/getAllQRInfo', [LearningInfoController::class, 'getQrInfoAssociations']);
